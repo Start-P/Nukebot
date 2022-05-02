0cf540d245ccdc3dbbd9b4e613689674492a61f4
Start-P <92899739+Start-P@users.noreply.github.com>
2022-05-03 03:03:47 +0900

    Update spammessage.py

--- spammessage.py
+++ spammessage.py
@@ -116,6 +116,5 @@ https://media.discordapp.net/attachments/827417203116736543/898535131244032000/2
 https://media.discordapp.net/attachments/827417203116736543/881391737329815602/1629512134335.jpg
 https://media.discordapp.net/attachments/827417203116736543/914420044618924123/1638083850255.jpg
 https://bit.ly/2ZFzDrB
-https://discord.gg/st7GGSBFqn
 @everyone
 """
